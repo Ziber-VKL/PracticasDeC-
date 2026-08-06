592008ffd8ccc260589c8f1418ca8add4a63eba4
Ziber-VKL <abel.espino@utp.ac.pa>
2026-08-06 11:44:01 -0500

    Dia#3 C#

---
+++ AdivinarNumero/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace AdivinarNumero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            String respuesta = "S";
+
+            while (respuesta.ToUpper() == "S")
+            {
+                int numeroRamdon = aleatorio.Next(1, 101);
+                bool adivino = false;
+                int intentos = 0;
+
+                while(intentos < 10 && !adivino)
+                {
+                    Console.Write("Ingrese un numero: ");
+                    int numero = Convert.ToInt32(Console.ReadLine());
+
+                    intentos++;
+
+                    if (numero > numeroRamdon)
+                    {
+                        Console.WriteLine("Demasiado alto");
+                    }
+                    else if (numero < numeroRamdon)
+                    {
+                        Console.WriteLine("Demasiado bajo");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Felicidades, adivinaste el numero");
+                        Console.WriteLine($"El numero correcto era: {numeroRamdon}");
+
+                        Console.WriteLine($"Cantidad de intentos: {intentos}");
+                        adivino = true;
+                    }
+                }
+
+                if (!adivino)
+                {
+                    Console.WriteLine("Game Over");
+                    Console.WriteLine($"El numero correcto era: {numeroRamdon}");
+                }
+                Console.Write("Desea jugar de nuevo? (Y/N): ");
+                respuesta = Console.ReadLine();
+            }
+            Console.WriteLine("Gracias por jugar");
+        }
+        
+    }
+}
---
+++ AdvenOfCode2015/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace AdvenOfCode2015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string instrucciones = File.ReadAllText("input.txt");
+            int piso = 0;
+
+           for (int i =0; i < instrucciones.Length; i++)
+            {
+                if (instrucciones[i] == '(')
+                    piso++;
+
+                else if (instrucciones[i] == ')')
+                    piso--;
+
+                if (piso == -1)
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+
+            }
+        }
+    }
+}
