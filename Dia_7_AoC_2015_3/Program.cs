d1f53c2c81697f9f28a1d9ca8590195930a6cfd9
Ziber-VKL <abel.espino@utp.ac.pa>
2026-08-11 11:22:00 -0500

    Dia#7

---
+++ Dia_7_AoC_2015_3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using static System.Console;
+
+namespace Dia_7_AoC_2015_3;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string input = File.ReadAllText("input.txt");
+
+        int x = 0;
+        int y = 0;
+
+        HashSet<string> visitadas = new HashSet<string>();
+
+        visitadas.Add($"{x} {y}");
+
+        foreach(char cambio in input)
+        {
+            switch(cambio)
+            {
+                case '^':
+                    y++;
+                    break;
+
+                case 'v':
+                    y--;
+                    break;
+
+                case '>':
+                    x++;
+                    break;
+
+                case '<':
+                    x--;
+                    break;
+            }
+
+            visitadas.Add($"{x} {y}");
+        }
+        WriteLine(visitadas.Count);
+    }
+}
---
+++ Dia_7_AoC_2015_3_P2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using static System.Console;
+namespace Dia_7_AoC_2015_3_P2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string input = File.ReadAllText("input.txt");
+
+        int santaX = 0;
+        int santaY = 0;
+
+        int robotX = 0;
+        int robotY = 0;
+
+        HashSet<string> visitadas = new HashSet<string>();
+
+        visitadas.Add("0,0");
+
+        for(int i = 0; i < input.Length; i++)
+        {
+            char cambio = input[i];
+
+            if(i % 2 == 0)
+            {
+                switch (cambio)
+                {
+                    case '^':
+                        santaY++;
+                        break;
+
+                    case 'v':
+                        santaY--;
+                        break;
+
+                    case '>':
+                        santaX++;
+                        break;
+
+                    case '<':
+                        santaX--;
+                        break;
+                }
+                visitadas.Add($"{santaX},{santaY}");
+            }
+            else
+            {
+                switch (cambio)
+                {
+                    case '^':
+                        robotY++;
+                        break;
+
+                    case 'v':
+                        robotY--;
+                        break;
+
+                    case '>':
+                        robotX++;
+                        break;
+
+                    case '<':
+                        robotX--;
+                        break;
+                }
+                visitadas.Add($"{robotX},{robotY}");
+            }
+        }
+        WriteLine(visitadas.Count);
+    }
+}
