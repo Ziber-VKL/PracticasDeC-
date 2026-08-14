0acbff0bd8b40f48020f8143dd2cd23f31d4f78f
Ziber-VKL <abel.espino@utp.ac.pa>
2026-08-14 15:15:05 -0500

    Dia #10

---
+++ Dia4_AoC_2015/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+namespace Dia4_AoC_2015;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string puzzleKey = "yzbqklnj";
+
+        using MD5 mD5 = MD5.Create();
+
+        int numero = 1;
+
+        while (true)
+        {
+            string input = puzzleKey + numero;
+
+            byte[] bytes = Encoding.UTF8.GetBytes(input);
+
+            byte[] hash = mD5.ComputeHash(bytes);
+
+            string hashString = Convert.ToHexString(hash).ToLower();
+
+            if (hashString.StartsWith("000000"))
+            {
+                Console.WriteLine($"{numero}");
+                break;
+            }
+         numero++;
+        }
+    }
+}
